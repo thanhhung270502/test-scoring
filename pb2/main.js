b5cc8d4e5e50ec5e45a0bc777d593aab3d3be799
thanhhung270502 <thanhhung270502@gmail.com>
2023-04-06 23:11:53 +0700

    Update header

--- pb2/main.js
+++
@@ -1,9 +0,0 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-
-const form_start = $('.start');
-const form = $('.form-toggle');
-
-form_start.onclick = () => {
-    form.classList.toggle('hide');
-}
